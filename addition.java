09d5905b8e6840172ea43c065cebe44b80f2d393
ShouravMolla99 <shouravmolla1999@gmail.com>
2022-12-11 14:21:18 +0600

    add new item

---
+++ addition.java
@@ -0,0 +1,16 @@
+public class addition {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 15;
+        int sum1 = x + y;
+        int num1, num2, result;
+        num1 = 35;
+        num2 = 44;
+        result = num1 + num2;
+
+        int first = 23, second = 13, addition = first + second;
+        System.out.println(addition);
+        System.out.println(sum1);
+        System.out.println(result);
+    }
+}
---
+++ arrayFirst.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class arrayFirst {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 23;
+        numbers[2] = 15;
+        numbers[3] = 10;
+        System.out.println(Arrays.toString(numbers));
+    }
+}
